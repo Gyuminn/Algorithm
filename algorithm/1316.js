4d8eaa95573de2807fbf36f58090b8274584f04d
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-04-10 14:16:12 +0900

    백준 1316.js 풀이

--- algorithm/1316.js
+++ algorithm/1316.js
@@ -7,6 +7,7 @@ const rl = readline.createInterface({
 
 let input = [];
 let count = 0;
+let countGroupWord = 0;
 
 function lineHandler(line) {
   count++;
@@ -21,8 +22,30 @@ function lineHandler(line) {
 function closeHandler() {
   for (let i = 1; i < count; i++) {
     const strArray = input[i].split("");
-  }
+    let groupWordChecker = [];
+    let isGroupWord = true;
 
+    for (let j = 0; j < strArray.length; j++) {
+      if (groupWordChecker.indexOf(strArray[j]) === -1) {
+        // 해당 문자가 없는 경우에는 그냥 push
+        groupWordChecker.push(strArray[j]);
+      } else {
+        // 만약 이미 나왔던 문자인데, 마지막 인덱스가 아닌 경우
+        if (
+          groupWordChecker.indexOf(strArray[j]) !==
+          groupWordChecker.length - 1
+        ) {
+          isGroupWord = false;
+          break;
+        }
+      }
+    }
+
+    if (isGroupWord) {
+      countGroupWord++;
+    }
+  }
+  console.log(countGroupWord);
   process.exit();
 }
 
