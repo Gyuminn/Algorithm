21f18609a8d929f64cfde65bc17ac1737cd631e8
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-04-12 16:50:21 +0900

    백준 10250.js 수정 및 주석 추가

--- algorithm/10250.js
+++ algorithm/10250.js
@@ -23,12 +23,14 @@ function lineHandler(line) {
 function closeHandler() {
   for (let i = 1; i < input.length; i++) {
     const [H, W, N] = input[i].split(" ");
-    const frontDigits = N % H;
+    const frontDigits = N % H === 0 ? H : N % H;
+    // 앞의 두 자리수는 나눈 나머지이고 만약 나머지가 0이라면 H 값이 된다.
     const backDigits =
       Math.floor(N % H) === 0 ? Math.floor(N / H) : Math.floor(N / H) + 1;
-
-    const answer = `${frontDigits === 0 ? H : frontDigits}${
+    // 뒤의 두 자리수는 나눈 나머지가 0이라면 몫, 아니라면 몫 + 1이다.
+    const answer = `${frontDigits}${
       backDigits < 10 ? `0${backDigits}` : backDigits
+      // 뒤의 두 자리수가 10보다 작다면 앞에 0을 붙여주어야 한다.
     }`;
     answerList.push(answer);
   }
