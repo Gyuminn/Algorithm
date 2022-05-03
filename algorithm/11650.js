be556b6e8a81559460222e5ee8cf7506660cab2d
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-05-04 00:50:57 +0900

    백준 11650.js 풀이

---
+++ algorithm/11650.js
@@ -0,0 +1,34 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+const lineHandler = (line) => {
+  input.push(line.split(" "));
+
+  if (input.length > Number(input[0])) {
+    input.shift();
+    sortCoordinate(input);
+    rl.close();
+  }
+};
+
+const sortCoordinate = (arr) => {
+  arr.sort((a, b) => {
+    if (a[0] === b[0]) {
+      return a[1] - b[1];
+    }
+    return a[0] - b[0];
+  });
+  console.log(arr.map((element) => element.join(" ")).join(`\n`));
+};
+
+const closeHandler = () => {
+  process.exit();
+};
+rl.on("line", lineHandler);
+rl.on("close", closeHandler);
