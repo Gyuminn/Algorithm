803fa2e6fb63f9f6d6685131f8b528ee4364b302
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-05-30 20:45:10 +0900

    백준 17478.js 풀이

--- algorithm/17478.js
+++ algorithm/17478.js
@@ -5,28 +5,30 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-const firstQuestion = `어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.\n`;
-
-const depth = "";
 const underline = "____";
-const recursiveAnswer = `${depth}"재귀함수가 뭔가요?"
-${depth}"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.
-${depth}마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.
-${depth}그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어."`;
-
-const lastAnswer = `${depth}"재귀함수가 뭔가요?"
-${depth}`;
 
 const lineHandler = (line) => {
   const n = Number(line);
-  getAnswer();
+  console.log(`어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.`);
+  getAnswer(1, n);
   rl.close();
 };
 
-const getAnswer = (n) => {
-  let answer = firstQuestion.concat(recursiveAnswer);
+const getAnswer = (N, n) => {
+  const recursiveUnderline = underline.repeat(N - 1);
+  if (N > n) {
+    console.log(`${recursiveUnderline}"재귀함수가 뭔가요?"
+${recursiveUnderline}"재귀함수는 자기 자신을 호출하는 함수라네"
+${recursiveUnderline}라고 답변하였지.`);
+    return;
+  }
 
-  console.log(answer);
+  console.log(`${recursiveUnderline}"재귀함수가 뭔가요?"
+${recursiveUnderline}"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.
+${recursiveUnderline}마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.
+${recursiveUnderline}그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어."`);
+  getAnswer(N + 1, n);
+  console.log(recursiveUnderline + "라고 답변하였지.");
 };
 
 const closeHandler = () => {
