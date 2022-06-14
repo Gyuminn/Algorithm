9e9c7a11892decd3ee1d2850151544241d285022
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-06-14 15:35:31 +0900

    백준 1463.js 주석 추가

--- baekjoon/1463.js
+++ baekjoon/1463.js
@@ -11,13 +11,16 @@ const lineHandler = (line) => {
   const DP = new Array(n + 1).fill(0);
 
   for (let i = 2; i <= n; i++) {
+    // 1을 뺀 경우의 최솟값
     DP[i] = DP[i - 1] + 1;
 
+    // 2로 나눴을 경우의 최솟값
     if (i % 2 === 0) {
       DP[i] = Math.min(DP[i], DP[i / 2] + 1);
     }
 
     if (i % 3 === 0) {
+      // 3으로 나눴을 경우의 최솟값.
       DP[i] = Math.min(DP[i], DP[i / 3] + 1);
     }
   }
