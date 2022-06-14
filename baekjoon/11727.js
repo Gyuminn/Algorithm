2613eb9de2c7900d9e0a96d4d22c4c1f9cccd7de
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-06-14 16:48:05 +0900

    백준 11727.js 풀이

---
+++ baekjoon/11727.js
@@ -0,0 +1,31 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const lineHandler = (line) => {
+  const n = Number(line);
+  getNumCase(n);
+  rl.close();
+};
+
+const getNumCase = (num) => {
+  const memo = {
+    1: 1,
+    2: 3,
+  };
+
+  for (let i = 3; i <= num; i++) {
+    memo[i] = (memo[i - 1] + 2 * memo[i - 2]) % 10007;
+  }
+  console.log(memo[num]);
+};
+
+const closeHandler = () => {
+  process.exit();
+};
+
+rl.on("line", lineHandler);
+rl.on("close", closeHandler);
