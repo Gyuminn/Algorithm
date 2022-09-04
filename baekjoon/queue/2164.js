ac06f6fc449cc4a89bb241d781d910c2ec68a565
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-09-04 16:22:01 +0900

    백준 2164.js Doubliy Linked List 를 이용해 구현

--- baekjoon/queue/2164.js
+++ baekjoon/queue/2164.js
@@ -5,15 +5,68 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
+class Node {
+  constructor(item) {
+    this.item = item;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+// Queue 자료구조를 위한 Doubly LinkedList
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  pushNode(item) {
+    const newNode = new Node(item);
+
+    if (this.head === null) {
+      this.head = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+    }
+
+    this.tail = newNode;
+    this.length += 1;
+
+    return newNode;
+  }
+
+  shiftNode() {
+    this.head = this.head.next;
+    this.head.prev = null;
+    this.length -= 1;
+  }
+
+  getHead() {
+    return this.head.item;
+  }
+
+  getSize() {
+    return this.length;
+  }
+}
+
 const lineHandler = (line) => {
   const N = Number(line);
-  const arr = Array.from({ length: N }, (v, i) => i + 1);
 
-  while (arr.length !== 1) {
-    arr.shift();
-    arr.push(arr.shift());
+  const cards = new LinkedList();
+
+  for (let i = 1; i <= N; i++) {
+    cards.pushNode(i);
   }
-  console.log(arr[0]);
+
+  while (cards.getSize() !== 1) {
+    cards.shiftNode();
+    cards.pushNode(cards.getHead());
+    cards.shiftNode();
+  }
+  console.log(cards.getHead());
   process.exit();
 };
 rl.on("line", lineHandler);
