e19a3c3b3f139b7ef8944ffce28f9a758cd955f1
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-09-09 11:10:24 +0900

    백준 13305.js 풀이

--- baekjoon/greedy/13305.js
+++ baekjoon/greedy/13305.js
@@ -7,13 +7,21 @@ const rl = readline.createInterface({
 
 let input = [];
 const lineHandler = (line) => {
-  input.push(line.split(" ").map((v) => +v));
+  input.push(line.split(" ").map((v) => BigInt(v)));
 
   if (input.length > 2) {
     const dist = input[1];
-    const cost = input[2];
+    const costs = input[2];
+    let curPrice = costs[0];
+    let cost = 0n;
 
-    console.log(dist, cost);
+    for (let i = 0; i < costs.length - 1; i++) {
+      cost += curPrice * dist[i];
+      if (curPrice > costs[i + 1]) {
+        curPrice = costs[i + 1];
+      }
+    }
+    console.log(String(cost));
     process.exit();
   }
 };
