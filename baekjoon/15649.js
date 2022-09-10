e9d4fcb3b8157873ae35f468b440b3b91e4d7e0b
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-09-10 16:17:26 +0900

    백준 15649 재풀이

--- baekjoon/15649.js
+++ baekjoon/15649.js
@@ -7,30 +7,28 @@ const rl = readline.createInterface({
 
 const lineHandler = (line) => {
   const [N, M] = line.split(" ").map((v) => +v);
-  solution(N, M, 0, 0, "");
-  rl.close();
-};
-
-const solution = (N, M, count, used, val) => {
-  const arr = [];
-
-  for (let i = 1; i <= N; i++) {
-    arr.push(String(i));
-  }
-
-  if (count == M) {
-    console.log(val);
-    return false;
-  } else {
-    for (let i = 0; i < N; i++) {
-      if (used & (1 << i)) continue;
-      solution(N, M, count + 1, used | (1 << i), val + arr[i] + " ");
+  const tmp = Array.from({ length: M }, () => 0);
+  const answer = [];
+  const visited = Array.from({ length: N + 1 }, () => false);
+  const DFS = (L) => {
+    if (L === M) {
+      // 얕은 배열 복사를 방지하기 위해 slice를 사용해야 한다.
+      // 그렇지 않으면 마지막 답만 출력되게 됨.
+      answer.push(tmp.slice());
+    } else {
+      for (let i = 1; i <= N; i++) {
+        if (!visited[i]) {
+          visited[i] = true;
+          tmp[L] = i;
+          DFS(L + 1);
+          visited[i] = false;
+        }
+      }
     }
-  }
-};
+  };
 
-const closeHandler = () => {
+  DFS(0);
+  console.log(answer.map((v) => v.join(" ")).join(`\n`));
   process.exit();
 };
 rl.on("line", lineHandler);
-rl.on("close", closeHandler);
