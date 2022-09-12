35bd2a9e6c3e10374586d6a25b6b582ca177e8a6
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-09-12 14:40:42 +0900

    백준 15666.js 풀이

---
+++ baekjoon/15666.js
@@ -0,0 +1,35 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+const lineHandler = (line) => {
+  input.push(line.split(" ").map((v) => +v));
+
+  if (input.length > 1) {
+    const [N, M] = input[0];
+    const arr = input[1];
+    arr.sort((a, b) => a - b);
+    const tmp = Array.from({ length: M }, () => 0);
+    const answer = [];
+
+    const DFS = (L, idx) => {
+      if (L === M) {
+        answer.push(tmp.slice());
+      } else {
+        for (let i = idx; i < N; i++) {
+          tmp[L] = arr[i];
+          DFS(L + 1, i);
+        }
+      }
+    };
+
+    DFS(0, 0);
+    console.log([...new Set(answer.map((v) => v.join(" ")))].join(`\n`));
+    process.exit();
+  }
+};
+rl.on("line", lineHandler);
