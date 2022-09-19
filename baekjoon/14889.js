d9d75abe48f2520cbb6a5d72738223d767ba5434
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-09-19 14:47:00 +0900

    백준 14889.js 풀이

--- baekjoon/14889.js
+++ baekjoon/14889.js
@@ -1,19 +1,54 @@
-const readline = require("readline");
+function sol(input) {
+  const [[N], ...stats] = input;
+  const halfN = N / 2;
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+  const visited = Array.from({ length: N }, () => 0);
+  let min = Number.MAX_SAFE_INTEGER;
 
-let input = [];
+  const DFS = (L, start) => {
+    if (L === halfN) {
+      const sTeam = [];
+      const lTeam = [];
+      let sSum = (lSum = 0);
+
+      for (let i = 0; i < N; i++) {
+        if (visited[i] === 1) sTeam.push(i);
+        else lTeam.push(i);
+      }
+
+      for (let i = 0; i < halfN; i++) {
+        for (let j = i + 1; j < halfN; j++) {
+          sSum += stats[sTeam[i]][sTeam[j]] + stats[sTeam[j]][sTeam[i]];
+          lSum += stats[lTeam[i]][lTeam[j]] + stats[lTeam[j]][lTeam[i]];
+        }
+      }
+      min = Math.min(min, Math.abs(sSum - lSum));
+    } else {
+      for (let i = start; i < N; i++) {
+        // 여기서 visited 체크는 순회 조건이 아니라 위에서 팀을 나누는데 사용됨.
+        visited[i] = 1;
+        DFS(L + 1, i + 1);
+        visited[i] = 0;
+      }
+    }
+  };
+  DFS(0, 0);
+  return min;
+}
+
+const input = [];
 const lineHandler = (line) => {
   input.push(line.split(" ").map((v) => +v));
 
   if (input.length > input[0][0]) {
-    const [[N], ...S] = input;
-
-    console.log(N, S);
+    console.log(sol(input));
     process.exit();
   }
 };
-rl.on("line", lineHandler);
+
+require("readline")
+  .createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  })
+  .on("line", lineHandler);
