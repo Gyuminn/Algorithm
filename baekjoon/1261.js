b821de05044bcdcd68f3ea9cd124b1862bbb6884
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-09-30 19:36:32 +0900

    백준 1261.js 입력 받는 부분에서 런타임 에러 발생, fix 완료

--- baekjoon/1261.js
+++ baekjoon/1261.js
@@ -1,14 +1,13 @@
 const sol = (input) => {
-  const [[M, _, N], ...board] = input;
+  const [[M, N], ...board] = input;
 
   const BFS = (sx, sy) => {
-    const visited = Array.from(Array(N), () => Array(M).fill(0));
-    const dx = [-1, 0, 1, 0];
-    const dy = [0, 1, 0, -1];
-
     const deque = [];
     deque.push([sx, sy, 0]);
+    const visited = Array.from(Array(N), () => Array(M).fill(0));
     visited[sx][sy] = 1;
+    const dx = [-1, 0, 1, 0];
+    const dy = [0, 1, 0, -1];
 
     while (deque.length) {
       const [x, y, cnt] = deque.shift();
@@ -16,16 +15,15 @@ const sol = (input) => {
 
       for (let k = 0; k < 4; k++) {
         const [nx, ny] = [x + dx[k], y + dy[k]];
-        // 아래와 같은 방식으로 풀면 visited[nx][nx] 에서 -1을 참조하기 때문에
-        // 런타임 에러가 발생한다...
-        if (nx >= 0 && ny >= 0 && nx < N && ny < M && visited[nx][ny] === 0) {
-          visited[nx][ny] = 1;
-          if (board[nx][ny] === 1) {
-            board[nx][ny] = 0;
-            deque.push([nx, ny, cnt + 1]);
-          } else {
-            deque.unshift([nx, ny, cnt]);
-          }
+        if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
+        if (visited[nx][ny] === 1) continue;
+
+        visited[nx][ny] = 1;
+        if (board[nx][ny] === 1) {
+          board[nx][ny] = 0;
+          deque.push([nx, ny, cnt + 1]);
+        } else {
+          deque.unshift([nx, ny, cnt]);
         }
       }
     }
@@ -41,8 +39,13 @@ require("readline")
     output: process.stdout,
   })
   .on("line", (line) => {
-    input.push(line.split("").map((v) => +v));
-    if (input.length > input[0][2]) {
+    if (input.length === 0) {
+      input.push(line.split(" ").map((v) => +v));
+    } else {
+      input.push(line.split("").map((v) => +v));
+    }
+
+    if (input.length > input[0][1]) {
       console.log(sol(input));
       process.exit();
     }
