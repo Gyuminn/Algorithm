e83c510944a41123b9ac020aef9bdb4eec8e47d5
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-10-02 17:08:20 +0900

    백준 1707.js 이분 그래프 재풀이

--- baekjoon/1707.js
+++ baekjoon/1707.js
@@ -1,47 +1,18 @@
-const readline = require("readline");
+const answer = [];
+const sol = (input) => {
+  const [n, m] = input[0];
+  const visited = Array.from({ length: n + 1 }, () => 0);
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+  // graph 관계를 위한 인접 리스트 생성
+  const graph = Array.from(Array(n + 1), () => Array());
+  input.shift();
 
-let count = 0;
-let input = [];
-
-rl.on("line", (line) => {
-  if (!count) {
-    count = Number(line);
-  } else {
-    input.push(line);
-    if (input.length == Number(input[0].split(" ")[1]) + 1) {
-      main();
-      input = [];
-    }
+  for (let [a, b] of input) {
+    graph[a].push(b);
+    graph[b].push(a);
   }
-});
 
-const main = () => {
-  const graph = [];
-  let answer = "YES";
-
-  let n, m;
-
-  input.forEach((el, i) => {
-    const edge = el.split(" ").map(Number);
-    if (i === 0) {
-      [n, m] = edge;
-      for (let i = 1; i <= n; i++) {
-        graph[i] = [];
-      }
-    } else {
-      const [from, to] = edge;
-      graph[from].push(to);
-      graph[to].push(from);
-    }
-  });
-
-  const visited = new Array(n + 1).fill(0);
-  const dfs = (start) => {
+  const BFS = (start) => {
     const queue = [];
     queue.push(start);
 
@@ -50,38 +21,69 @@ const main = () => {
 
       for (let i = 0; i < graph[cur].length; i++) {
         const next = graph[cur][i];
-        if (!visited[next]) {
+        if (visited[next] === 0) {
           if (visited[cur] === 1) {
             visited[next] = 2;
           } else {
             visited[next] = 1;
           }
           queue.push(next);
-        } else if (visited[cur] === visited[next]) {
+        } else if (visited[next] === visited[cur]) {
+          // 더 이상 검색할 필요가 없다.
           return;
         }
       }
     }
   };
 
+  // 분리된 그래프가 있을 수 있으므로
   for (let i = 1; i <= n; i++) {
-    if (!visited[i]) {
+    if (visited[i] === 0) {
       visited[i] = 1;
-      dfs(i);
+      BFS(i);
     }
   }
 
+  // 인접한 정점끼리의 숫자가 같은지 비교
   let flag = false;
   loop1: for (let i = 1; i <= n; i++) {
     for (let j = 0; j < graph[i].length; j++) {
       if (visited[i] === visited[graph[i][j]]) {
-        console.log("NO");
+        answer.push("NO");
         flag = true;
         break loop1;
       }
     }
   }
+
   if (!flag) {
-    console.log(answer);
+    answer.push("YES");
   }
 };
+
+// 여러 개의 테스트 케이스를 입력받는 코드
+let cnt = 0;
+let input = [];
+
+require("readline")
+  .createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  })
+  .on("line", (line) => {
+    if (!cnt) {
+      cnt = +line;
+    } else {
+      input.push(line.split(" ").map((v) => +v));
+      if (input.length == input[0][1] + 1) {
+        sol(input);
+        input = [];
+        cnt--;
+      }
+    }
+
+    if (cnt === 0) {
+      console.log(answer.join(`\n`));
+      process.exit();
+    }
+  });
