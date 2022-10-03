4214b72ea47070205ce9fd04c9f5b8343f95c8d0
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-10-03 13:44:23 +0900

    백준 7576.js 연결리스트로 queue를 구현하여 시간초과 해결

--- baekjoon/7576.js
+++ baekjoon/7576.js
@@ -1,24 +1,61 @@
+class Node {
+  constructor(item) {
+    this.item = item;
+    this.next = null;
+  }
+}
+
+// LinkedList로 Queue 구현
+class Queue {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  pushNode(item) {
+    const newNode = new Node(item);
+
+    if (this.head == null) {
+      this.head = newNode;
+    } else {
+      this.tail.next = newNode;
+    }
+
+    this.tail = newNode;
+    this.length += 1;
+  }
+
+  shiftNode() {
+    const shiftItem = this.head;
+    this.head = this.head.next;
+    this.length -= 1;
+    return shiftItem.item;
+  }
+}
+
 const sol = (input) => {
   const [[M, N], ...board] = input;
 
   const dx = [1, 0, -1, 0];
   const dy = [0, 1, 0, -1];
 
-  const queue = [];
+  const queue = new Queue();
+
   let ripeDay = 0;
 
   // 익은 토마토(1)를 찾아서 queue에 넣어준다.
   for (let i = 0; i < N; i++) {
     for (let j = 0; j < M; j++) {
       if (board[i][j] === 1) {
-        queue.push([i, j]);
+        queue.pushNode([i, j]);
       }
     }
   }
 
   const BFS = () => {
     while (queue.length) {
-      const [x, y] = queue.shift();
+      const [x, y] = queue.shiftNode();
 
       for (let k = 0; k < 4; k++) {
         const [nx, ny] = [x + dx[k], y + dy[k]];
@@ -26,7 +63,7 @@ const sol = (input) => {
         // 지도를 벗어나지 않고 익지않은 토마토(0)라면
         if (nx >= 0 && nx < N && ny >= 0 && ny < M && board[nx][ny] === 0) {
           board[nx][ny] = board[x][y] + 1;
-          queue.push([nx, ny]);
+          queue.pushNode([nx, ny]);
         }
       }
     }
