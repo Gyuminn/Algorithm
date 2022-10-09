0979dd7d6df4b0caf5b98009dd108f13fb855334
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-10-09 16:31:52 +0900

    백준 2146.js 주석 추가

--- baekjoon/2146.js
+++ baekjoon/2146.js
@@ -4,16 +4,16 @@ const sol = (input) => {
   const dy = [0, 1, 0, -1];
 
   let visited = Array.from(Array(N), () => Array(N).fill(0));
-  let isLandCnt = 0;
+  let landNum = 0;
 
   function check(x, y) {
     return 0 <= x && x < N && 0 <= y && y < N;
   }
 
-  function BFS_addLandNumber(sx, sy, isLandCnt) {
+  function BFS_addLandNumber(sx, sy, landNum) {
     const queue = [];
     visited[sx][sy] = 1;
-    board[sx][sy] = isLandCnt;
+    board[sx][sy] = landNum;
     queue.push([sx, sy]);
 
     while (queue.length) {
@@ -24,7 +24,7 @@ const sol = (input) => {
 
         if (check(nx, ny) && board[nx][ny] && !visited[nx][ny]) {
           visited[nx][ny] = 1;
-          board[nx][ny] = isLandCnt;
+          board[nx][ny] = landNum;
           queue.push([nx, ny]);
         }
       }
@@ -34,19 +34,20 @@ const sol = (input) => {
   for (let i = 0; i < N; i++) {
     for (let j = 0; j < N; j++) {
       if (board[i][j] && !visited[i][j]) {
-        isLandCnt += 1;
-        BFS_addLandNumber(i, j, isLandCnt);
+        landNum += 1;
+        BFS_addLandNumber(i, j, landNum);
       }
     }
   }
 
-  function BFS(isLandCnt) {
+  function BFS(landNum) {
     const queue = [];
     visited = Array.from(Array(N), () => Array(N).fill(0));
 
+    // 같은 대륙에 있는 모든 땅들을 queue에 넣고 시작.
     for (let i = 0; i < N; i++) {
       for (let j = 0; j < N; j++)
-        if (board[i][j] == isLandCnt) {
+        if (board[i][j] == landNum) {
           visited[i][j] = 1;
           queue.push([i, j]);
         }
@@ -57,16 +58,18 @@ const sol = (input) => {
       let qlen = queue.length;
 
       while (qlen--) {
-        let cur = queue.shift();
-        let x = cur[0];
-        let y = cur[1];
+        const [x, y] = queue.shift();
 
-        for (let i = 0; i < 4; i++) {
-          let nx = x + dx[i];
-          let ny = y + dy[i];
+        for (let k = 0; k < 4; k++) {
+          const [nx, ny] = [x + dx[k], y + dy[k]];
 
+          // 지도를 벗어나는 경우
           if (!check(nx, ny)) continue;
-          if (board[nx][ny] !== 0 && board[nx][ny] !== isLandCnt) return cnt;
+
+          // 육지이고, 이동하기 전 대륙의 번호와 다른 번호라면 cnt 반환
+          if (board[nx][ny] !== 0 && board[nx][ny] !== landNum) return cnt;
+
+          // 바다라면 큐에 넣어주고 계속 진행
           if (board[nx][ny] === 0 && !visited[nx][ny]) {
             visited[nx][ny] = 1;
             queue.push([nx, ny]);
@@ -78,7 +81,7 @@ const sol = (input) => {
   }
 
   let ans = Infinity;
-  for (let i = 1; i <= isLandCnt; i++) {
+  for (let i = 1; i <= landNum; i++) {
     ans = Math.min(ans, BFS(i));
   }
 
