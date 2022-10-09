e60e32b15622b3978bc4c4e734cafaa68f31b2a8
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-10-09 16:50:32 +0900

    백준 1991.js 입출력 방식 변경

--- baekjoon/1991.js
+++ baekjoon/1991.js
@@ -1,14 +1,16 @@
 const sol = (input) => {
-  const N = +input[0];
-  input = input.slice(1);
+  const [N, ...relation] = input;
+
   const tree = {};
+
   for (let i = 0; i < N; i++) {
-    const [node, left, right] = input[i].split(" ");
+    const [node, left, right] = relation[i];
     tree[node] = [left, right]; // tree의 키 값으로 노드를 저장하고, 값으로는 left, right가 담긴 배열을 저장한다.
   }
 
   let result = "";
 
+  // 전위순회는 루트부터 기록을 시작하므로, 재귀의 맨 앞에서 우선 기록한다.
   function preorder(node) {
     if (node === ".") return;
     const [lt, rt] = tree[node];
@@ -16,8 +18,8 @@ const sol = (input) => {
     preorder(lt);
     preorder(rt);
   }
-  // 전위순회는 루트부터 기록을 시작하므로, 재귀의 맨 앞에서 우선 기록한다.
 
+  // 중위순회는 왼쪽-루트-오른쪽 순이므로, 왼쪽의 재귀가 끝난 시점에서 기록한다.
   function inorder(node) {
     if (node === ".") return;
     const [lt, rt] = tree[node];
@@ -25,8 +27,8 @@ const sol = (input) => {
     result += node;
     inorder(rt);
   }
-  // 중위순회는 왼쪽-루트-오른쪽 순이므로, 왼쪽의 재귀가 끝난 시점에서 기록한다.
 
+  // 후위순회는 왼쪽-오른쪽-루트 순이므로, 왼쪽과 오른쪽 재귀가 끝난 시점에서 기록한다.
   function postorder(node) {
     if (node === ".") return;
     const [lt, rt] = tree[node];
@@ -34,7 +36,6 @@ const sol = (input) => {
     postorder(rt);
     result += node;
   }
-  // 후위순회는 왼쪽-오른쪽-루트 순이므로, 왼쪽과 오른쪽 재귀가 끝난 시점에서 기록한다.
 
   preorder("A");
   result += "\n";
@@ -45,14 +46,18 @@ const sol = (input) => {
   return result;
 };
 
-// 백준에서 입력을 받는 코드
 const input = [];
 require("readline")
   .createInterface(process.stdin, process.stdout)
   .on("line", (line) => {
-    input.push(line);
-  })
-  .on("close", () => {
-    console.log(sol(input));
-    process.exit();
+    if (input.length === 0) {
+      input.push(+line);
+    } else {
+      input.push(line.split(" "));
+    }
+
+    if (input.length > input[0]) {
+      console.log(sol(input));
+      process.exit();
+    }
   });
