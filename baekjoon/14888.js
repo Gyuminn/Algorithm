22d0f23b1f73a8fb033476d7e5025ca9af31417c
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-10-17 21:38:19 +0900

    백준 14888.js 풀이

---
+++ baekjoon/14888.js
@@ -0,0 +1,52 @@
+const sol = (input) => {
+  const N = input[0];
+  const nums = input[1];
+  const operators = input[2];
+
+  const calc = [
+    (a, b) => a + b,
+    (a, b) => a - b,
+    (a, b) => a * b,
+    (a, b) => ~~(a / b),
+  ];
+
+  let max = Number.MIN_SAFE_INTEGER;
+  let min = Number.MAX_SAFE_INTEGER;
+
+  const dfs = (L, temp) => {
+    if (L === N - 1) {
+      max = Math.max(max, temp);
+      min = Math.min(min, temp);
+    } else {
+      for (let i = 0; i < 4; i++) {
+        if (!operators[i]) continue;
+        operators[i]--;
+        dfs(L + 1, calc[i](temp, nums[L + 1]));
+        operators[i]++;
+      }
+    }
+  };
+
+  dfs(0, nums[0]);
+  let answer = [];
+  answer.push(max);
+  answer.push(min);
+
+  return answer.join(`\n`);
+};
+
+const input = [];
+
+require("readline")
+  .createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  })
+  .on("line", (line) => {
+    input.push(line.split(" ").map((v) => +v));
+
+    if (input.length > 2) {
+      console.log(sol(input));
+      process.exit();
+    }
+  });
