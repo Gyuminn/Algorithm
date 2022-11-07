48b49ae8b124780fadeaff43a011572e09e4f30b
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-11-07 22:42:22 +0900

    백준 15658.js 풀이

--- baekjoon/14225.js
+++ baekjoon/14225.js
@@ -1,31 +1,25 @@
 const sol = (input) => {
-  const N = +input[0];
-  const S = input[1].split(" ").map(Number);
-  const MAX_SUM = 2000001;
-  const sumArr = new Array(MAX_SUM).fill(0);
-  const pick = [];
+  const [[N], ...S] = input;
+  S.sort((a, b) => a - b);
 
-  function dfs(L) {
+  const dfs = (L) => {
     if (L === N) {
-      const sum = pick.reduce((sum, val) => sum + val, 0);
-      sumArr[sum] = 1;
       return;
+    } else {
     }
-    pick.push(S[L]);
-    dfs(L + 1);
-    pick.pop();
-    dfs(L + 1);
-  }
-  dfs(0);
-
-  for (let i = 1; i < MAX_SUM; i++) if (!sumArr[i]) return i;
+  };
 };
 
 const input = [];
 require("readline")
   .createInterface(process.stdin, process.stdout)
   .on("line", (line) => {
-    input.push(line);
+    input.push(line.split(" ").map((v) => +v));
+
+    if (input.length == 2) {
+      console.log(sol(input));
+      process.exit();
+    }
   })
   .on("close", () => {
     console.log(sol(input));
