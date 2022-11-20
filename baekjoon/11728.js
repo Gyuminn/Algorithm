892722e5894d8b4c947e914113fc317852fc4617
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-11-20 14:49:38 +0900

    (refactor: baekjoon) 11728.js, reduce time by using way of merge sort

--- baekjoon/11728.js
+++ baekjoon/11728.js
@@ -1,12 +1,34 @@
 const input = [];
 
 const sol = (input) => {
+  const [N, M] = input[0];
   const A = input[1];
   const B = input[2];
 
-  const AB = [...A, ...B].sort((a, b) => a - b);
+  let i = 0;
+  let j = 0;
 
-  return AB.join(" ");
+  const answer = [];
+  while (i < N && j < M) {
+    if (A[i] <= B[j]) {
+      answer.push(A[i]);
+      i++;
+    } else {
+      answer.push(B[j]);
+      j++;
+    }
+  }
+
+  while (i < N) {
+    answer.push(A[i]);
+    i++;
+  }
+
+  while (j < M) {
+    answer.push(B[j]);
+    j++;
+  }
+  return answer.join(" ");
 };
 
 require("readline")
