99448a2590951ab353316b16ffc3c45560ac7a4b
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-11-21 20:44:58 +0900

    (feat: baekjoon) 16946.js, solve by grouping & copy input

--- baekjoon/16946.js
+++ baekjoon/16946.js
@@ -40,27 +40,27 @@ class Queue {
 
 const sol = (input) => {
   const [[N, M], ...board] = input;
+  // 2차원 배열의 깊은 복사
+  const maskBoard = board.map((row) => row.slice());
 
-  const zerosIdx = [];
+  // 테스트 케이스로 인해서 원본 배열을 수정하지 말고, 복사해서 쓰자.
+  const answer = board.map((row) => row.slice());
 
   const dx = [-1, 0, 1, 0];
   const dy = [0, 1, 0, -1];
 
-  const answer = Array.from(Array(N), () => Array(M).fill(0));
+  const zerosOfGroup = new Map();
+  const zeroVisited = Array.from(Array(N + 1), () => Array(M + 1).fill(0));
 
-  for (let i = 0; i < N; i++) {
-    for (let j = 0; j < M; j++) {
-      if (board[i][j] === 1) zerosIdx.push([i, j]);
-    }
-  }
+  let group = 0;
+  let zeroCnt = 1;
 
-  const bfs = (sx, sy) => {
+  const checkMask = (sx, sy, zeroCnt) => {
     const queue = new Queue();
-    queue.pushNode([sx, sy]);
 
-    const visited = Array.from(Array(N), () => Array(M).fill(0));
-    visited[sx][sy] = 1;
-    let cnt = 1;
+    queue.pushNode([sx, sy]);
+    zeroVisited[sx][sy] = 1;
+    maskBoard[sx][sy] = group;
 
     while (queue.getSize()) {
       const [x, y] = queue.shiftNode();
@@ -68,23 +68,54 @@ const sol = (input) => {
       for (let k = 0; k < 4; k++) {
         const [nx, ny] = [x + dx[k], y + dy[k]];
 
-        if (nx < 0 || nx >= N || ny < 0 || ny >= M || board[nx][ny]) continue;
-        if (visited[nx][ny]) continue;
+        if (nx < 0 || nx >= N || ny < 0 || ny >= M || maskBoard[nx][ny] !== 0)
+          continue;
+
+        if (zeroVisited[nx][ny]) continue;
+
+        zeroVisited[nx][ny] = 1;
+        maskBoard[nx][ny] = group;
         queue.pushNode([nx, ny]);
-        visited[nx][ny] = 1;
-        cnt++;
+        zeroCnt++;
       }
     }
-    return cnt;
+    zerosOfGroup.set(group, zeroCnt);
   };
-  zerosIdx.forEach((zeros) => {
-    const [x, y] = zeros;
-    board[x][y] = 0;
-    answer[x][y] = bfs(x, y);
-    board[x][y] = 1;
-  });
 
-  return answer.map((ar) => ar.join("")).join(`\n`);
+  const countZero = (sx, sy) => {
+    const groupVisited = new Set();
+    for (let i = 0; i < 4; i++) {
+      const [nx, ny] = [sx + dx[i], sy + dy[i]];
+      if (nx < 0 || nx >= N || ny < 0 || ny >= M || board[nx][ny] !== 0)
+        continue;
+
+      const group = maskBoard[nx][ny];
+      if (groupVisited.has(group)) continue;
+      answer[sx][sy] += zerosOfGroup.get(group);
+      groupVisited.add(group);
+    }
+    answer[sx][sy] = answer[sx][sy] % 10;
+  };
+
+  for (let i = 0; i < N; i++) {
+    for (let j = 0; j < M; j++) {
+      if (maskBoard[i][j] === 0) {
+        group++;
+        checkMask(i, j, zeroCnt);
+        zeroCnt = 1;
+      }
+    }
+  }
+
+  for (let i = 0; i < N; i++) {
+    for (let j = 0; j < M; j++) {
+      if (board[i][j] == 1) {
+        countZero(i, j);
+      }
+    }
+  }
+
+  return answer.map((row) => row.join("")).join(`\n`);
 };
 const input = [];
 require("readline")
