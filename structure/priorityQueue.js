e64e16ce9f952eb6b353db42b148b392ff29f4dc
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2022-12-11 11:58:47 +0900

    (feat: data structure) priority Queue extends MinHeap

---
+++ structure/priorityQueue.js
@@ -0,0 +1,9 @@
+class PriorityQueue extends Heap {
+  constructor() {
+    super();
+  }
+
+  enqueue = (priority, value) => this.insert(priority, value);
+  dequeue = () => this.remove();
+  isEmpty = () => this.heap.length <= 0;
+}
