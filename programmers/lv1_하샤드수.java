f214ef9def949e7d2833e4edd6b70425473c20ce
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2023-08-16 20:28:20 +0900

    prg lv1 java 하샤드 수

---
+++ programmers/lv1_하샤드수.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(int x) {
+        
+        int xLen = (int) (Math.log10(x) + 1); // 숫자형의 길이를 구하는 방법.
+        int digitSum = 0;
+        int originX = x;
+        
+        ArrayList<Integer> arrDigitNum = new ArrayList<>();
+        
+        // 숫자의 각 자리수를 ArrayList에 담는 방법.
+        while(x > 0) {
+            System.out.println(x);
+            arrDigitNum.add(x % 10);
+            x /= 10;
+        }
+        
+        for(int digit: arrDigitNum) {
+            digitSum += digit;
+        }
+        System.out.println(arrDigitNum.toString());
+        
+        boolean answer = false;
+        if (originX % digitSum == 0) {
+            answer = true;    
+        }
+        
+        return answer;
+    }
+}
